3f0551490bfc5fef064e5510178fe83781673d3e
Paul Dickens <Paul.Dickens@resmed.com.au>
2020-10-19 21:26:05 +1100

    squarify box before saving to ROIs

--- face-landmarks-detection/src/mediapipe-facemesh/pipeline.ts
+++ face-landmarks-detection/src/mediapipe-facemesh/pipeline.ts
@@ -271,8 +271,9 @@ export class Pipeline {
             const scaledBox =
                 scaleBoxCoordinates(predictionBoxCPU, scaleFactor as Coord2D);
             const enlargedBox = enlargeBox(scaledBox);
+            const squarifiedBox = squarifyBox(enlargedBox);
             return {
-              ...enlargedBox,
+              ...squarifiedBox,
               landmarks: prediction.landmarks.arraySync() as Coords3D
             };
           });
@@ -329,7 +330,7 @@ export class Pipeline {
 
         const boxCPU = {startPoint: box.startPoint, endPoint: box.endPoint};
         const face: tf.Tensor4D =
-            cutBoxFromImageAndResize(squarifyBox(boxCPU), rotatedImage, [
+            cutBoxFromImageAndResize(boxCPU, rotatedImage, [
               this.meshHeight, this.meshWidth
             ]).div(255);
 
@@ -406,8 +407,9 @@ export class Pipeline {
 
         const landmarksBox = enlargeBox(
             this.calculateLandmarksBoundingBox(transformedCoordsData));
+        const squarifiedLandmarksBox = squarifyBox(landmarksBox)
         this.regionsOfInterest[i] = {
-          ...landmarksBox,
+          ...squarifiedLandmarksBox,
           landmarks: transformedCoords.arraySync() as Coords3D
         };
 
